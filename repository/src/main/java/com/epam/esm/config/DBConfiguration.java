54e30fe9804ae411108a310ae5c3699e071bcd0b
Nadzeya Zmushka <nadinkaby@gmail.com>
2021-09-30 19:09:47 +0300

    [Task-2]:build.gradle fixed, repository config added

---
+++ repository/src/main/java/com/epam/esm/config/DBConfiguration.java
@@ -0,0 +1,25 @@
+package com.epam.esm.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@ComponentScan("com.epam.esm")
+public class DBConfiguration {
+
+    @Bean
+    public JdbcTemplate getTemplate() {
+        return new JdbcTemplate(getDatasource());
+    }
+
+    @Bean
+    public HikariDataSource getDatasource() {
+        HikariConfig config = new HikariConfig("db.properties");
+        return new HikariDataSource(config);
+    }
+
+}
