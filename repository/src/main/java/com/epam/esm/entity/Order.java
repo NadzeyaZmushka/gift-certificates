7965ad135553bfa2e1b85a692fccd97c89e02be7
Nadzeya Zmushka <nadinkaby@gmail.com>
2022-01-21 13:32:16 +0300

    remove unused imports

--- repository/src/main/java/com/epam/esm/entity/Order.java
+++ repository/src/main/java/com/epam/esm/entity/Order.java
@@ -5,13 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
