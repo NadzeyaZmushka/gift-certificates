34dee8edc372c1b7f5a78386aac8b8b924bbbe0b
Nadzeya Zmushka <nadinkaby@gmail.com>
2022-01-23 15:01:40 +0300

    edit Application

--- web/src/main/java/com/epam/esm/Application.java
+++ web/src/main/java/com/epam/esm/Application.java
@@ -3,14 +3,21 @@ package com.epam.esm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = "com.epam.esm.**")
 @EntityScan(basePackages = {"com.epam.esm.**"})
-public class Application {
+public class Application extends SpringBootServletInitializer implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
 
